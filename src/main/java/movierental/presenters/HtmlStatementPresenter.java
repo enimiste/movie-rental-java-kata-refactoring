727c56c1ffe9fe2b392807552d8e987b8319039d
Enimiste <nouni.elbachir@gmail.com>
2024-05-09 17:13:32 +0200

    F : Add Html statement presenter (Customer class)

---
+++ src/main/java/movierental/presenters/HtmlStatementPresenter.java
@@ -0,0 +1,34 @@
+package movierental.presenters;
+
+import movierental.Statement;
+
+public class HtmlStatementPresenter implements StatementPresenter {
+    @Override
+    public String present(Statement statement) {
+        String result = makeStatementHeader(statement.customerName());
+
+        for (Statement.RentalLine rental : statement.rentalLines()) {
+            result += makeStatementRentalLine(rental.movieTitle(), rental.amount());
+        }
+
+        // add footer lines
+        result += makeStatementFooter(statement.totalAmount(), statement.totalFrequentRenterPoints());
+
+        return result;
+    }
+
+    private static String makeStatementFooter(double totalAmount, int frequentRenterPoints) {
+        return "</table>\n" +
+                "<p>Amount owed is <em>" + totalAmount + "</em></p>\n" +
+                "<p>You earned <em>" + frequentRenterPoints + "</em> frequent renter points</p>\n";
+    }
+
+    private static String makeStatementRentalLine(String rentalMovieTitle, double amount) {
+        return "  <tr><td>" + rentalMovieTitle+ "</td><td>" + amount + "</td></tr>\n";
+    }
+
+    private static String makeStatementHeader(String customerName) {
+        return "<h1>Rental Record for <em>" + customerName + "</em></h1>\n" +
+                "<table>\n";
+    }
+}
--- src/test/java/movierental/CustomerTest.java
+++ src/test/java/movierental/CustomerTest.java
@@ -2,6 +2,7 @@ package movierental;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 
+import movierental.presenters.HtmlStatementPresenter;
 import movierental.presenters.StringStatementPresenter;
 import org.junit.Test;
 
@@ -38,7 +39,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void test() {
+    public void test_string_presentation() {
         Customer customer = new Customer("Bob");
         customer.addRental(new Rental(new Movie("Jaws", Movie.PriceCode.REGULAR), 2));
         customer.addRental(new Rental(new Movie("Golden Eye", Movie.PriceCode.REGULAR), 3));
@@ -48,8 +49,7 @@ public class CustomerTest {
         customer.addRental(new Rental(new Movie("Toy Story", Movie.PriceCode.CHILDRENS), 4));
 
         StringStatementPresenter stringStatementPresenter = new StringStatementPresenter();
-        String expected = "" +
-                "Rental Record for Bob\n" +
+        String expected = "Rental Record for Bob\n" +
                 "\tJaws\t2.0\n" +
                 "\tGolden Eye\t3.5\n" +
                 "\tShort New\t3.0\n" +
@@ -62,4 +62,29 @@ public class CustomerTest {
         assertEquals(expected, stringStatementPresenter.present(customer.statement()));
     }
 
+    @Test
+    public void test_html_presentation() {
+        Customer customer = new Customer("Bob");
+        customer.addRental(new Rental(new Movie("Jaws", Movie.PriceCode.REGULAR), 2));
+        customer.addRental(new Rental(new Movie("Golden Eye", Movie.PriceCode.REGULAR), 3));
+        customer.addRental(new Rental(new Movie("Short New", Movie.PriceCode.NEW_RELEASE), 1));
+        customer.addRental(new Rental(new Movie("Long New", Movie.PriceCode.NEW_RELEASE), 2));
+        customer.addRental(new Rental(new Movie("Bambi", Movie.PriceCode.CHILDRENS), 3));
+        customer.addRental(new Rental(new Movie("Toy Story", Movie.PriceCode.CHILDRENS), 4));
+
+        HtmlStatementPresenter htmlStatementPresenter = new HtmlStatementPresenter();
+        String expected = "<h1>Rental Record for <em>Bob</em></h1>\n" +
+                "<table>\n" +
+                "  <tr><td>Jaws</td><td>2.0</td></tr>\n" +
+                "  <tr><td>Golden Eye</td><td>3.5</td></tr>\n" +
+                "  <tr><td>Short New</td><td>3.0</td></tr>\n" +
+                "  <tr><td>Long New</td><td>6.0</td></tr>\n" +
+                "  <tr><td>Bambi</td><td>1.5</td></tr>\n" +
+                "  <tr><td>Toy Story</td><td>3.0</td></tr>\n" +
+                "</table>\n" +
+                "<p>Amount owed is <em>19.0</em></p>\n" +
+                "<p>You earned <em>7</em> frequent renter points</p>\n";
+
+        assertEquals(expected, htmlStatementPresenter.present(customer.statement()));
+    }
 }
